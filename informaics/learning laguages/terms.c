e12745e17e9ecd8cd2fc6a13247fe76fb2899ee6
Mike <testev08@gmail.com>
2023-11-25 09:55:31 +0000

    new task about terms

--- informaics/learning laguages/terms.c
+++ informaics/learning laguages/terms.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 #include <math.h>
 // https://informatics.msk.ru/mod/statements/view.php?id=276#1
+
 void maximum()
 {
     int a;
@@ -13,8 +14,23 @@ void maximum()
         max =b;
     printf("%d", max);
 }
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=253#1
+
+void leap_year()
+{
+    int a;
+    int c;
+    c = a % 4;
+    scanf("%d", a);
+    if (c= 0)
+        printf("YES");
+    else
+        printf("NO");
+}
+
 int main()
 {
-    maximum();
+    leap_year();
     return 0;
 }
