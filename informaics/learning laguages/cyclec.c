d03c0c4d0e56aa1801175347f82291517846df01
Mike <testev08@gmail.com>
2023-12-08 19:38:34 +0000

    changes based on comments in cycles.c

--- informaics/learning laguages/cyclec.c
+++ informaics/learning laguages/cyclec.c
@@ -1,14 +1,15 @@
 #include <math.h>
 #include <stdio.h>
+
 // https://informatics.msk.ru/mod/statements/view.php?id=278#1
-void sum_of_squares ()
+void sum_of_squares()
 {
     int n;
     int i;
     int sum;
 
     scanf("%d", &n);
-    for(i = 1; i<=n; i++)
+    for(i = 1; i <= n; i++)
     {
         sum += i * i;
     }
@@ -24,7 +25,7 @@ void factorial()
     result=1;
 
     scanf("%d", &N);
-    for (i=1; i<=N; i++)
+    for (i = 1; i <= N; i++)
     {
         result = result * i;
     }
@@ -32,15 +33,15 @@ void factorial()
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=278&chapterid=352#1
-void degree ()
+void degree()
 {
     int N;
     int i;
     int result;
-    result =1;
+    result = 1;
 
     scanf("%d", &N);
-    for (i =1; i<=N; i++)
+    for (i = 1; i <= N; i++)
     {
         result = result * 2;
     }
@@ -48,18 +49,18 @@ void degree ()
 }
 
 // https://informatics.msk.ru/mod/statements/view.php?id=278&chapterid=319#1
-void geometrical_progression ()
+void geometrical_progression()
 {
     int a;
     int n;
     int result;
     int sum;
     int i;
-    result =1;
-    sum =1;
+    result = 1;
+    sum = 1;
 
     scanf("%d%d", &a, &n);
-    for (i = 1; i<=n; i++)
+    for (i = 1; i <= n; i++)
     {
         result = result*n;
         sum += result;
@@ -68,16 +69,16 @@ void geometrical_progression ()
 }
 
 //https://informatics.msk.ru/mod/statements/view.php?id=249#1
-void list_of_squares ()
+void list_of_squares()
 {
     int N;
     int i;
-    i =1;   
+    i = 1;   
 
     scanf("%d", &N);
-    while(i*i <N)
+    while(i * i <N)
     { 
-        printf("%d", i*i);
+        printf("%d", i * i);
         i = i + 1;
     }
 }
