d0c352f74f3034deb9d9ecedc2792c9d4eca8d37
Mike <testev08@gmail.com>
2024-01-04 18:15:59 +0000

    changes in massives.c

--- informaics/learning laguages/massives.c
+++ informaics/learning laguages/massives.c
@@ -1,16 +1,14 @@
 #include <stdio.h> 
-#include <math.h>
 
 // https://informatics.msk.ru/mod/statements/view.php?id=208#1
-void A0_A2_A4 ()
+void A0_A2_A4()
 {
     int N, i, j;
     
-
     scanf("%d", &N);
     int A[N];
 
-    for( i = 0; i < N; i++)
+    for ( i = 0; i < N; i++)
     {
         scanf ("%d", &A[i]);
     } 
