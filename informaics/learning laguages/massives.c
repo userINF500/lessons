6b009155b30d15edd2478988400d96288f5ae14d
Mike <testev08@gmail.com>
2024-01-05 19:38:53 +0000

    new task massives.c

--- informaics/learning laguages/massives.c
+++ informaics/learning laguages/massives.c
@@ -20,8 +20,31 @@ void A0_A2_A4()
     
 }
 
+// https://informatics.msk.ru/mod/statements/view.php?id=208&chapterid=64#1
+void write_even_numbers()
+{
+    int N, i, j;
+    
+    scanf("%d", &N);
+    int arr[N];
+
+    for ( i = 0; i < N; i++)
+    {
+        scanf ("%d", &arr[i]);
+    } 
+
+    for (j = 0; j < N; j ++)
+    {
+        if (arr[j] % 2  == 0)
+        {
+            printf("%d ", arr[j]);
+        }
+
+    }
+}
+
 int main ()
 {
-    A0_A2_A4();
+    write_even_numbers();
     return 0;
 }
